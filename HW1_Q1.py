b9cf6ac81e8be9cd319fb0b4bbef7184957c3f6b
sandunijayasundara <nishadikajayasundara@gmail.com>
2020-10-28 01:19:58 +0530

    Update HW1_Q1.py

--- HW1_Q1.py
+++ HW1_Q1.py
@@ -249,3 +249,35 @@ if __name__ == '__main__':
     tree = etree.ElementTree(etree.fromstring(xml))
     depth(tree.getroot(), -1)
     print(maxdepth)
+    
+    
+""""----------------------- Q1_Part_12 1 Standardize Mobile Number Using Decorators ------------------"""
+def wrapper(f):
+    def fun(l):
+        # complete the function
+        f(['+91 ' + c[-10:-5] + ' ' + c[-5:] for c in l])
+    return fun
+
+@wrapper
+def sort_phone(l):
+    print(*sorted(l), sep='\n')
+
+if __name__ == '__main__':
+    l = [input() for _ in range(int(input()))]
+    sort_phone(l) 
+
+""""----------------------- Q1_Part_12 2 Decoraters Name Directory ------------------"""
+def person_lister(f):
+    def inner(people):
+        # complete the function
+        return map(f, sorted(people, key=lambda x: int(x[2])))
+
+    return inner
+
+@person_lister
+def name_format(person):
+    return ("Mr. " if person[3] == "M" else "Ms. ") + person[0] + " " + person[1]
+
+if __name__ == '__main__':
+    people = [input().split() for i in range(int(input()))]
+    print(*name_format(people), sep='\n')
